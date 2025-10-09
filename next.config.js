51583f03e4043de77690ffce05152f15afb89bfe
Yayifications <gospmav@gmail.com>
2025-10-09 15:16:02 -0600

    changed port

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configurar el puerto para desarrollo
+  experimental: {
+    turbo: {
+      rules: {},
+    },
+  },
+};
+
+module.exports = nextConfig;
